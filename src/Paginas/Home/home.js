8446f091628c55f9ad0be15be56ec39be3365247
Jardel <tecjardelv@gmail.com>
2023-04-01 15:37:22 -0300

    estilizacao da pagina home

--- src/Paginas/Home/home.js
+++ src/Paginas/Home/home.js
@@ -25,7 +25,7 @@ export default function Home() {
                 </section>
                 <section className='container-eventos'>
                     <div className='descricao-eventos'>
-                        <h2>NOSSOS EVENTOS</h2>
+                     <h2>NOSSOS EVENTOS</h2>
                         <span>Delicias com sorvete!</span>
                         <p>Mais do que uma sorveteria, uma extensão da sua casa! Estamos aqui prontinhos para te atender e oferecer os melhores eventos com os melhores sorvete da sua vida! Venha nos conhecer e passar um tempo aqui com a gente.</p>
                     </div>
